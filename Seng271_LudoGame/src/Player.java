1134ddab65163ff75641b69a1188dda778cf08ae
Andrew Hobden <andrew@hoverbear.org>
2013-07-22 09:32:10 -0700

    Added skeleton methods to Player for #6.

--- Seng271_LudoGame/src/Player.java
+++ Seng271_LudoGame/src/Player.java
@@ -1,4 +1,18 @@
-
+/* 
+ * 
+ */
 public class Player {
-
+	// Feel free to change these to public as you see fit.
+	private Strategy strategy;
+	private GoalField goalField;
+	private HomeField homeField;
+	private Pawn[] Pawns;
+	
+	public void planMove(){
+		
+	}
+	
+	public void doMove() {
+		
+	}
 }
